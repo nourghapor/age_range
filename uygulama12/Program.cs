3485f41c191df04b6ef974da997ac92c58bb65b0
nourghapor <67339923+nourghapor@users.noreply.github.com>
2020-07-18 18:38:44 +0300

    Age-Range-

---
+++ uygulama12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uygulama12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad; 
+            string soyad;
+            Console.WriteLine("adınız giriniz:");
+           ad= Console.ReadLine();
+            Console.WriteLine("soyadınızı giriniz:");
+            soyad=Console.ReadLine();
+            Console.Write("Merhaba "+ ad +" "+ soyad);
+            Console.ReadLine();
+            int yas;
+            Console.WriteLine("yaşınızı giriniz:");
+            yas =Convert.ToInt16(Console.ReadLine());
+            if (yas < 18)
+            Console.Write("18 yaşları ve altı girmiyor");
+            Console.WriteLine("kapanıyor");
+            Environment.Exit(0);
+
+         
+        }
+    }
+}
